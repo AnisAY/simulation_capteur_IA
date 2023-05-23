70ba2d62893cf923b464a262786a25c9d6255158
Jubadj <adjaoud.juba.17@gmail.com>
2023-05-24 00:47:51 +0100

    Docker mis à jour

--- projet/kafka_topic.py
+++ projet/kafka_topic.py
@@ -3,24 +3,30 @@ from pyspark.sql import SparkSession
 from pyspark.sql import *
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import *
-
-
-
+import pandas as pd
+import numpy as np
+import datetime
+from kafka import *
+from minio import Minio
+from minio.error import S3Error
+import datetime
+import json
+import random
 
 def main():
-  # Connect to Kafka producer
-  producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    # Connect to Kafka producer
+    producer = KafkaProducer(bootstrap_servers='localhost:2909')
 
-  # Connect to local Spark instance
-  #spark = SparkSession.builder.appName("CapteursDataStorage").master("spark://spark-master:7077").getOrCreate()
+    # Connect to local Spark instance
+    #spark = SparkSession.builder.appName("CapteursDataStorage").master("spark://spark-master:7077").getOrCreate()
 
-  spark = (SparkSession.builder.appName("CapteursDataStorage")
+    spark = (SparkSession.builder.appName("CapteursDataStorage")
            .master("spark://spark-master:7077")
            .getOrCreate())
 
-  # Define schema for Capteurs data
-  # ["timestamp", "entrance_amount", "exit_amount", "temperature", "humidity", "parking_entrance", "parking_exit", "parking_actual_vehicle"]
-  schema = StructType([
+    # Define schema for Capteurs data
+    # ["timestamp", "entrance_amount", "exit_amount", "temperature", "humidity", "parking_entrance", "parking_exit", "parking_actual_vehicle"]
+    schema = StructType([
       StructField("timestamp", TimestampType(), True),
       StructField("temperature", DoubleType(), True),
       StructField("humidity", DoubleType(), True),
@@ -29,34 +35,27 @@ def main():
       StructField("parking_entrance", IntegerType(), True),
       StructField("parking_exit", IntegerType(), True),
       StructField("parking_actual_vehicle", IntegerType(), True),
-  ])
+    ])
 
-  # Define Kafka topic to read from
-  topic = "capteur"
+    # Define Kafka topic to read from
+    topic = "capteur"
 
-  df = spark \
+    df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("kafka.bootstrap.servers", "localhost:2909") \
     .option("subscribe", topic) \
     .option("startingOffsets", "earliest") \
     .load() \
     .select(from_json(col("value").cast("string"), schema).alias("data")) \
     .select("data.*")
 
-  # # Read data from Kafka topic
-  # df = spark \
-  #     .readStream \
-  #     .format("kafka") \
-  #     .option("kafka.bootstrap.servers", "localhost:9092") \
-  #     .option("subscribe", topic) \
-  #     .load()
+    query = df \
+    .writeStream \
+    .format("parquet") \
+    .option("path", "s3a://donnees-kafka/processed_data") \
+    .option("checkpointLocation", "/path/to/checkpoint/dir") \
+    .start()
 
-# query = df \
-#    .writeStream \
-#    .format("parquet") \
-#    .option("path", "capteurs_data_storage") \
-#    .option("checkpointLocation", "capteurs_data_storage_checkpoint") \
-#    .start()
 if __name__ == "__main__":
-  main()
+    main()
